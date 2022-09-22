860ccdbc890727270436161b304d7bac029c291a
harutoakira <thaidangminhhoang19@gmail.com>
2022-09-23 02:01:09 +0700

    DesignFrontEnd

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ namespace FactoryMethod
         {
             InitializeComponent();
             button1.Click += button1_Click;
+            // panel1.Paint += panel1_Paint;
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -19,5 +20,13 @@ namespace FactoryMethod
             string voice2 = lancer.MyClass();
             MessageBox.Show(voice2);
         }
+    //     private void panel1_Paint(object sender, PaintEventArgs e)
+    //    {
+    //            ControlPaint.DrawBorder(e.Graphics, panel1.ClientRectangle,
+    //            Color.FromArgb(255,0,0), 2, ButtonBorderStyle.Solid, // left
+    //            Color.White, 1, ButtonBorderStyle.None, // top
+    //            Color.DimGray, 1, ButtonBorderStyle.None, // right
+    //            Color.FromArgb(255,0,0), 2, ButtonBorderStyle.Solid);// bottom
+    //    }
     }
 }
